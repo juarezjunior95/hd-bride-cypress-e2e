ef462d65492a498eba4b613a0d84c5ca766b13a4
Juares Cruz <juarescruz@MacBook-Air-de-Infobase.local>
2024-08-09 16:58:39 -0300

    Criando pipeline

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'qcpnyt',
   e2e: {
     baseUrl: "https://beta.hunterdouglas.com/",
     defaultCommandTimeout: 5000,
